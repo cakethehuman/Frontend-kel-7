f825ce90e9059f98b3ed16bbb289af5823be36b8
cakethehuman <willsonputra17@gmail.com>
2026-09-11 07:59:18 +0700

    Finish regssion with plot

---
+++ Tugas Kelompok 1/JS/graph-builder.js
@@ -0,0 +1,107 @@
+// graph-builder.js
+export class GraphBuilder {
+  constructor(canvas, { scale = 30 } = {}) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.originX = this.width / 2;
+    this.originY = this.height / 2;
+    this.scale = scale;
+  }
+
+  grid() {
+    const { ctx, width, height, scale } = this;
+    ctx.strokeStyle = "#ddd";
+    ctx.lineWidth = 1;
+
+    for (let x = 0; x <= width; x += scale) {
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, height);
+      ctx.stroke();
+    }
+
+    for (let y = 0; y <= height; y += scale) {
+      ctx.beginPath();
+      ctx.moveTo(0, y);
+      ctx.lineTo(width, y);
+      ctx.stroke();
+    }
+
+    return this;
+  }
+
+  axes() {
+    const { ctx, width, height, originX, originY, scale } = this;
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.moveTo(0, originY);
+    ctx.lineTo(width, originY);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(originX, 0);
+    ctx.lineTo(originX, height);
+    ctx.stroke();
+
+    ctx.fillStyle = "black";
+    ctx.font = "14px Arial";
+
+    for (let x = -10; x <= 10; x++) {
+      if (x === 0) continue;
+      const px = originX + x * scale;
+      ctx.fillText(x, px - 5, originY + 20);
+    }
+
+    for (let y = -10; y <= 10; y++) {
+      if (y === 0) continue;
+      const py = originY - y * scale;
+      ctx.fillText(y, originX + 8, py + 5);
+    }
+
+    return this;
+  }
+
+  point(x, y) {
+    const { pixelX, pixelY } = this._toPixel(x, y);
+    const { ctx } = this;
+
+    ctx.beginPath();
+    ctx.arc(pixelX, pixelY, 5, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+    return this;
+  }
+
+  line(x1, y1, x2, y2) {
+    const p1 = this._toPixel(x1, y1);
+    const p2 = this._toPixel(x2, y2);
+
+    this.ctx.strokeStyle = "blue";
+    this.ctx.lineWidth = 2;
+    this.ctx.beginPath();
+    this.ctx.moveTo(p1.pixelX, p1.pixelY);
+    this.ctx.lineTo(p2.pixelX, p2.pixelY);
+    this.ctx.stroke();
+
+    return this;
+  }
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+
+    return this;
+    }
+
+  _toPixel(x, y) {
+    return {
+      pixelX: this.originX + x * this.scale,
+      pixelY: this.originY - y * this.scale,
+    };
+  }
+}
--- Tugas Kelompok 1/JS/regression.js
+++ Tugas Kelompok 1/JS/regression.js
@@ -15,8 +15,8 @@ class Regression{
         let conv = 0;
         let varianceX = 0;
         for(let i = 0; i < this.x.length; i++){
-            conv += (x[i] - xMean) * (y[i] - yMean);
-            varianceX += (x[i] - xMean)**2;
+            conv += (this.x[i] - xMean) * (this.y[i] - yMean);
+            varianceX += (this.x[i] - xMean)**2;
         }
         let slope = conv / varianceX
         let intercept = yMean - slope * xMean;
@@ -24,7 +24,4 @@ class Regression{
     }
 }
 
-let x = [1,2];
-let y = [2,1];
-const r = new Regression(x,y);
-console.log(r.fit());
+export default Regression;
